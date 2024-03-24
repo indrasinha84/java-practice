c4a8f31ecfa5bcb4f920c70247e92541d0925723
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2024-03-24 20:19:28 +0530

    Tail rec implementation.

--- src/main/java/practice/Subsequence.java
+++ src/main/java/practice/Subsequence.java
@@ -11,6 +11,7 @@ public class Subsequence {
         var line = "MKHKGJJGGH;XMKGJJGRJGH";
         var bothParts = line.split(";");
         System.out.println(findSequence(bothParts[0], bothParts[1], "").getFirst());
+        System.out.println(findSequenceWithTailRec(bothParts[0], bothParts[1], ""));
     }
 
     static List<String> findSequence(String left, String right, String result) {
@@ -18,8 +19,7 @@ public class Subsequence {
             var retVal = new ArrayList<String>();
             retVal.add(result);
             return retVal;
-        }
-        else {
+        } else {
             return left.chars().mapToObj(Character::toString).flatMap(c -> {
                 var splitRight = right.split(c, 2);
                 if (splitRight.length > 1) {
@@ -30,6 +30,19 @@ public class Subsequence {
                     return retVal.stream();
                 }
             }).sorted(Comparator.comparing(String::length).reversed()).collect(Collectors.toList());
+        }
+    }
+
+    private static String findSequenceWithTailRec(String left, String right, String result) {
+        if (left.isEmpty() || right.isEmpty())
+            return result;
+        else {
+            var positionInRight = right.lastIndexOf(left.substring((left.length() - 1)));
+            if (positionInRight == -1)
+                return findSequenceWithTailRec(left.substring(0, left.length() - 1), right, result);
+            else
+                return findSequenceWithTailRec(left.substring(0, left.length() - 1), right.substring(0, positionInRight),
+                        left.substring(left.length() - 1) + result);
         }
     }
 }
