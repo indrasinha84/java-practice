d3c1fe6ef7006de0ee75f018cdb62f45f9ca4314
indrasinha84 <38633558+indrasinha84@users.noreply.github.com>
2024-03-24 19:49:48 +0530

    Code fix.

--- src/main/java/practice/Subsequence.java
+++ src/main/java/practice/Subsequence.java
@@ -8,21 +8,28 @@ import java.util.stream.Collectors;
 public class Subsequence {
 
     public static void main(String[] args) {
-        var line = "MKHKGJJGGH;XMKGJGRJH";
+        var line = "MKHKGJJGGH;XMKGJJGRJGH";
         var bothParts = line.split(";");
         System.out.println(findSequence(bothParts[0], bothParts[1], "").getFirst());
     }
 
     static List<String> findSequence(String left, String right, String result) {
-        return left.chars().mapToObj(Character::toString).flatMap(c -> {
-            var splitRight = right.split(c, 2);
-            if (splitRight.length > 1) {
-                return findSequence(left.split(c, 2)[1], splitRight[1], result + c).stream();
-            } else {
-                var retVal = new ArrayList<String>();
-                retVal.add(result);
-                return retVal.stream();
-            }
-        }).sorted(Comparator.comparing(String::length).reversed()).collect(Collectors.toList());
+        if (left.isEmpty()) {
+            var retVal = new ArrayList<String>();
+            retVal.add(result);
+            return retVal;
+        }
+        else {
+            return left.chars().mapToObj(Character::toString).flatMap(c -> {
+                var splitRight = right.split(c, 2);
+                if (splitRight.length > 1) {
+                    return findSequence(left.split(c, 2)[1], splitRight[1], result + c).stream();
+                } else {
+                    var retVal = new ArrayList<String>();
+                    retVal.add(result);
+                    return retVal.stream();
+                }
+            }).sorted(Comparator.comparing(String::length).reversed()).collect(Collectors.toList());
+        }
     }
 }
